65662ddf45666198c8d55866a1d0f4206051512d
Glyph <code@glyph.im>
2025-01-14 11:32:25 -0800

    skip discovery for now (does it work for zip files?) and just run the module manually
    This commit was sponsored by Brian Grohe, VM (Vicky) Brasseur,
    rockstar, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

--- PINPalMac/CiPINPal.py
+++ PINPalMac/CiPINPal.py
@@ -1,4 +1,6 @@
 from unittest import main
+# TODO: discover the whole package
+from pinpal.test import test_txtui
 
 if __name__ == "__main__":
-    main(module="pinpal.test", verbosity=2)
+    main(module=test_txtui.__name__, verbosity=2)
