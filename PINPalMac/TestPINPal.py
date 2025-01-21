4f03cf8fae86d32707d127379e912f71bc7390ee
Glyph <code@glyph.im>
2025-01-21 10:46:47 -0800

    fully realized test mode that doesn't put my production secrets on screen
    This commit was sponsored by Matt Campbell, jhulten, Brian Grohe, and
    my other patrons.  If you want to join them, you can support my work
    at https://glyph.im/patrons/.

---
+++ PINPalMac/TestPINPal.py
@@ -0,0 +1,8 @@
+import OpenSSL.SSL as _
+import service_identity as _
+import six.moves as _
+import _cffi_backend as _
+
+from pinpal.macos import testMain
+
+testMain.runMain()
--- src/pinpal/macos.py
+++ src/pinpal/macos.py
@@ -1,24 +1,27 @@
-from objc import object_property
+from __future__ import annotations
+
+from objc import object_property, IBOutlet
 from AppKit import NSApplication, NSNib, NSTableView, NSTableColumn
 from Foundation import NSObject
 from quickmacapp import Status, mainpoint  # , answer
 
 # from twisted.internet.defer import Deferred
+
+
 from twisted.internet.interfaces import IReactorTime
 
-from .app import PinPalApp
+from .app import PinPalApp, DEFAULT_SERVICE_NAME
 from .mem2 import Memorization2
 
 
 class MemorizationDataSource(NSObject):
     pinPalApp: PinPalApp = object_property()
     selectedRow: NSObject = object_property()
+    appOwner: PINPalAppOwner
+    appOwner = IBOutlet()
 
     def awakeFromNib(self) -> None:
-        loaded = PinPalApp.load()
-        if loaded is None:
-            loaded = PinPalApp.new()
-        self.pinPalApp = loaded
+        self.pinPalApp = self.appOwner.pinPalApp
 
     def tableViewSelectionDidChange_(self, notification: NSObject) -> None:
         self.selectedRow = self.tableView_objectValueForTableColumn_row_(
@@ -53,16 +56,29 @@ class PINPalAppOwner(NSObject):
     NIB owner for the application.
     """
 
+    pinPalApp: PinPalApp
 
-@mainpoint()
-def main(reactor: IReactorTime) -> None:
+    def initWithApp_(self, pinPalApp: PinPalApp) -> PINPalAppOwner:
+        self.pinPalApp = pinPalApp
+        return self.init()
+
+
+def maybeTestMain(reactor: IReactorTime, testMode: bool) -> None:
     """
     Run oldMain by default so I can keep using the app while I'm working on a
     radical refactor of the object model in newMain.
     """
     status = Status("🔑🦃🗝")
 
-    owner = PINPalAppOwner.alloc().init()
+    serviceName = (
+        DEFAULT_SERVICE_NAME if not testMode else f"testing.{DEFAULT_SERVICE_NAME}"
+    )
+
+    loaded = PinPalApp.load(serviceName)
+    if loaded is None:
+        loaded = PinPalApp.new(serviceName)
+
+    owner = PINPalAppOwner.alloc().initWithApp_(loaded)
 
     def sayHello() -> None:
         # Deferred.fromCoroutine(answer("hi"))
@@ -79,3 +95,13 @@ def main(reactor: IReactorTime) -> None:
             ("Quit", bye),
         ]
     )
+
+
+@mainpoint()
+def main(reactor: IReactorTime) -> None:
+    maybeTestMain(reactor, False)
+
+
+@mainpoint()
+def testMain(reactor: IReactorTime) -> None:
+    maybeTestMain(reactor, True)
