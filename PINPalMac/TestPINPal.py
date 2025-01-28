9646bacdc4762ea44b10615e0e409c67fbe0c780
Glyph <code@glyph.im>
2025-01-28 11:30:34 -0800

    add keyring entrypoints
    This commit was sponsored by Blaise Pabon, genehack, Carlton Gibson,
    and my other patrons.  If you want to join them, you can support my
    work at https://glyph.im/patrons/.

--- PINPalMac/TestPINPal.py
+++ PINPalMac/TestPINPal.py
@@ -1,7 +1,14 @@
 import OpenSSL.SSL as _
 import service_identity as _
-import six.moves as _
 import _cffi_backend as _
+
+# entrypoints declared from keyring, even the ones we don't need
+import keyring.backends.kwallet as _
+import keyring.backends.SecretService as _
+import keyring.backends.Windows as _
+import keyring.backends.chainer as _
+import keyring.backends.libsecret as _
+import keyring.backends.macOS as _
 
 from pinpal.macos import testMain
 
