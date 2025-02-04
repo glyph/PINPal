c20c4ee6728950981883cedbb995c33a071c1f55
Glyph <code@glyph.im>
2025-02-04 11:28:30 -0800

    make the difficulty-factor computation a bit more abstract, easier to re-use for tests
    This commit was sponsored by Matt Campbell, Cassie Jones, Blaise
    Pabon, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

--- src/pinpal/difficulty.py
+++ src/pinpal/difficulty.py
@@ -1,10 +1,11 @@
-
 from __future__ import annotations
 
 from dataclasses import dataclass
+from functools import cache
 from hashlib import scrypt
 from secrets import SystemRandom, token_bytes
 from time import time
+from typing import Callable
 
 oldDefaultScryptParams = {
     "n": str(0x4000),
@@ -14,6 +15,12 @@ oldDefaultScryptParams = {
 }
 
 sysrand = SystemRandom()
+
+
+THE_ONLY_VALUE_FOR_R = 8
+THE_ONLY_VALUE_FOR_P = 1
+ONE_HUNDRED_MILLISECONDS = 0.1
+
 
 @dataclass
 class SCryptParameters:
@@ -29,6 +36,18 @@ class SCryptParameters:
     def kdf(self, *, salt: bytes, password: bytes) -> bytes:
         return scrypt(
             password, salt=salt, r=self.r, p=self.p, n=self.n, maxmem=self.maxmem
+        )
+
+    @classmethod
+    def forNPower(
+        cls, nPower: int, r: int = THE_ONLY_VALUE_FOR_R, p: int = THE_ONLY_VALUE_FOR_P
+    ) -> SCryptParameters:
+        n = 1 << nPower
+        return cls(
+            r=r,
+            p=p,
+            n=n,
+            maxmem=_maxmemFor(n=n, r=THE_ONLY_VALUE_FOR_R),
         )
 
     @classmethod
@@ -55,42 +74,46 @@ class SCryptParameters:
         }
 
 
-determined: SCryptParameters | None = None
+def _maxmemFor(n: int, r: int = THE_ONLY_VALUE_FOR_R) -> int:
+    # documented in Node, but not Python, apparently: “It is an error when
+    # (approximately) 128 * N * r > maxmem. Default: 32 * 1024 * 1024.”
+    # https://nodejs.org/api/crypto.html#cryptoscryptsyncpassword-salt-keylen-options
+
+    # '* 2' added on the end here because we stil seem to bump into memory
+    # issues when set to exactly 128*n*r
+    return 128 * n * r * 2
 
 
-def determineScryptParameters(times: int = 10) -> SCryptParameters:
+@cache
+def determineScryptParameters(
+    times: int = 3, nPower: int = 13, debug: Callable[[str], None] = lambda _: None
+) -> SCryptParameters:
     """
     Determine an ideal value for `n` and `maxmem`, per U{this comment,
     <https://go-review.googlesource.com/c/crypto/+/67070/3/scrypt/scrypt.go#223>}
 
     'consider setting N to the highest power of 2 you can derive within 100
     milliseconds'
+
+    @param times: the number of invocations of scrypt() to average to get a
+        stable timing for its execution.
     """
-    global determined
-    if determined is not None:
-        return determined
     salt = token_bytes(16)
     password = token_bytes(16)
-    r = 8
-    p = 1
-    nPower = 13
-    n = 1 << nPower
 
     while True:
         then = time()
-        previousN = n
-        n = 1 << nPower
-        # documented in Node, but not Python, apparently: “It is an error when
-        # (approximately) 128 * N * r > maxmem. Default: 32 * 1024 * 1024.”
-        # https://nodejs.org/api/crypto.html#cryptoscryptsyncpassword-salt-keylen-options
-        maxmem = 128 * n * r * 2
-        # '* 2' added on the end here because we stil seem to bump into memory
-        # issues when set to exactly 128*n*r
-        for _ in range(times):
-            scrypt(salt=salt, password=password, r=r, p=p, n=n, maxmem=maxmem)
-
-        now = time()
-        if ((now - then) / times) > 0.1:
-            determined = SCryptParameters(r=r, p=p, n=previousN, maxmem=maxmem)
-            return determined
+        candidate = SCryptParameters.forNPower(nPower)
         nPower += 1
+        nextCandidate = SCryptParameters.forNPower(nPower)
+        for _ in range(times):
+            nextCandidate.kdf(salt=salt, password=password)
+        now = time()
+        avgTime = (now - then) / times
+        debug(f"tested candidate {nPower} at {avgTime}/sec")
+        if avgTime > ONE_HUNDRED_MILLISECONDS:
+            return candidate
+
+
+if __name__ == "__main__":
+    print(determineScryptParameters(debug=print))
