d922eb869769823311d8d7a0d473bb6f055b9052
Glyph <code@glyph.im>
2025-02-25 11:42:55 -0800

    correct filesystem path
    This commit was sponsored by Brian Grohe, Devin Prater, hacklschorsch,
    and my other patrons.  If you want to join them, you can support my
    work at https://glyph.im/patrons/.

--- src/pinpal/macos.py
+++ src/pinpal/macos.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from datetime import datetime
 from typing import Any
 from zoneinfo import ZoneInfo
@@ -156,8 +157,11 @@ def maybeTestMain(reactor: IReactorTime, testMode: bool) -> None:
     """
     Run main() normally, but if in a test-mode build, run testMain instead.
     """
+    from AppKit import NSBundle
+
+    b = NSBundle.mainBundle()
     from objc import pathForFramework, loadBundle
-    sparklePath = pathForFramework("Sparkle.framework")
+    sparklePath = pathForFramework(os.path.join(b.privateFrameworksPath(), "Sparkle.framework"))
     sparkleNS: dict[str, object] = {}
     loadBundle('Sparkle', sparkleNS, bundle_path=sparklePath)
     print(f"loaded: {sparkleNS}")
