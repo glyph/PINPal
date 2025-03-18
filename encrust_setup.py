8a97ad658f48585f592c826e573e3dca210c2fa0
Glyph <code@glyph.im>
2025-03-17 21:19:38 -0700

    update to use a new version of encrust that handles sparkle
    This commit was sponsored by Alex Scammon, Jason Walker, Mark
    Harviston, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

---
+++ encrust_setup.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from encrust.api import AppDescription, SparkleData
+
+description = AppDescription(
+    bundleID="im.glyph.and.this.is.pinpal",
+    bundleName="PINPal",
+    icnsFile=Path("icon.icns"),
+    mainPythonScript=Path("PINPalMac/PINPal.pyw"),
+    dataFiles=list(Path("PINPalMac").glob("*.xib")),
+    dockIconAtStart=False,
+    sparkleData=SparkleData.withConfig(
+        sparkleVersion="2.7.0",
+        publicEDKey="e4lwY+RAzYj1jgwjAqq6fIQJHpZVh/O2Od9aYSpY3CI=",
+        feedURL="https://www.glyph.im/apps/pinpal/updates/appcast.xml",
+        keychainAccount="im.glyph.and.this.is.my.sparkle.key",
+        localUpdatesFolder=(
+            Path.home() / "Storage" / "Sparkle" / "PINPal" / "Releases"
+        ),
+        remoteHost="public.glyph.im",
+        remotePath="/site/www.glyph.im/apps/pinpal/updates/",
+    ),
+).varyBundleForTesting()
